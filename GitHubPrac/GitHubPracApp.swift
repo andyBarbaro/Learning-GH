d4d6401a61a20d637eda447cbe945a03be526688
andyBarbaro <barbaro.3@osu.edu>
2022-07-05 18:15:26 -0400

    Initial Commit

---
+++ GitHubPrac/GitHubPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubPracApp.swift
+//  GitHubPrac
+//
+//  Created by Andy on 7/5/22.
+//
+
+import SwiftUI
+
+@main
+struct GitHubPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
